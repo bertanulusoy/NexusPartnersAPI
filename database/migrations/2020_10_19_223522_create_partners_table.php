80ed653edaa26cf7979da66789bb4e6d8155fc3f
Bertan Ulusoy <bertan.ulusoy@onur.net>
2020-10-20 02:16:19 +0300

    Partner model is created and migrated. nexus_partners_api database is created.

---
+++ database/migrations/2020_10_19_223522_create_partners_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePartnersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('partners', function (Blueprint $table) {
+            $table->id(10)->unsigned()->nullable(false);
+            $table->string('name', 100)->nullable(false);
+            $table->string('photo', 100)->nullable($value = true);
+            $table->timestamp('created_at')->nullable(false)->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->nullable($value = true);
+            //$table->timestamps();
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_unicode_ci';
+            $table->engine = 'InnoDB';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('partners');
+    }
+}
