4e2372b841520591469a0fc99662a39a86453378
Bertan Ulusoy <bertan.ulusoy@onur.net>
2020-10-20 02:47:33 +0300

    PartnerController created.

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Partner;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,21 +15,42 @@ use Illuminate\Support\Facades\Route;
 */
 
 
+/*Route::get('/partners', function() {
+    return ['message' => 'hello'];
+
+       $partner = Partner::create([
+        'name' => 'Bertan', 
+        'photo' => 'my photo'
+    ]);
+    return $partner;
+
+});*/
+
+
 // 1. GET /api/partners -- return all partners sorted by id field and paginate the results. The results should contain also the full URL to partner logo.
+Route::get('/partners', function() {
+});
 
 // 2. GET /api/partners?name=ab&per_page=3&page=2 -- return partners where name starts with prefix "ab".
+Route::get('/partners', function() {
+     
+});
 
 // 3. GET /api/partners/{id} -- return a single partner with the given id.
+Route::get('/partners/{id}', function() {
+     
+});
 
 // 4. POST /api/partners -- create a new partner, upload a partner logo, crop it to 100x100 pixels and store the filename in the field "photo". Implement a case insensitive rule that the "name" field can't contain the substring "Nexus" or its dashed derivatives (e.g. "N-exus", "nE-x----U-s").
+Route::post('/partners', function() {
+});
 
 // 5. PATCH /api/partners/{id} -- allow to update the partner info including the logo, the same validation rules apply as before.
+Route::patch('/partners/{id}', function() {
+});
 
 // 6. DELETE /api/partners/{id} -- delete the partner with the given id.
-
-
-Route::get('/partners', function() {
-    return ['message' => 'hello'];
+Route::delete('/partners/{id}', function() {
 });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
