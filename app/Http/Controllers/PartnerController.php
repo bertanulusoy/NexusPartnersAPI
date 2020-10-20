c9cdf26e405d188835e7aadd2056d50ef9f14e0a
Bertan Ulusoy <bertan.ulusoy@onur.net>
2020-10-20 23:54:09 +0300

    completed.

--- app/Http/Controllers/PartnerController.php
+++ app/Http/Controllers/PartnerController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Intervention\Image\ImageManagerStatic as Image;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
 
 use App\Models\Partner;
+
 
 class PartnerController extends Controller
 {
@@ -14,15 +18,16 @@ class PartnerController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index($Input)
+    public function index($name=null, $per_page=null, $page=null)
     {
         /*$partners = Partner::cursor()->filter(function($partner) {
             return $partner->name.star
         });*/
-        $name = $Input::get('name');
+        /*$name = $Input::get('name');
         $per_page = $Input::get('per_page');
-        $page = $Input::get('page');
-        
+        $page = $Input::get('page');*/
+        echo $name;
+         
         // pagination will be at that range
         if( isset($per_page) && isset($page) ){
             // if partner name set
@@ -60,7 +65,29 @@ class PartnerController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        if (preg_match("/n\-*e\-*x\-*u\-*s/i", $request->name)){
+            return "You cannot use 'Nexus' word and its derivative names for your partner name!";
+        } else {
+            if($request->has('photo')) {
+                // store partner logo image in public/images folder
+                $path = $request->photo->store('public/images');
+
+                // get partner logo image from public/images folder
+                $partner_logo = Storage::get($path);
+
+                // crop image and save it again with partners name
+                $file= Image::make(
+                    $partner_logo)
+                    ->crop('100','100')
+                    ->save($request->name.'.png'); 
+                // delete old saved image file.
+                Storage::delete($path);
+            }
+            $partner = new Partner;
+            $partner->name = $request->name;
+            $partner->photo = 'public/'.$request->name.'.png';
+            $partner->save();
+        }
     }
 
     /**
@@ -82,8 +109,36 @@ class PartnerController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
-    {
-        //
+    {   
+        if (preg_match("/n\-*e\-*x\-*u\-*s/i", $request->name)){
+            return "You cannot use 'Nexus' word and its derivative names for your partner name!";
+        } else {
+            // get partner object
+            $partner = Partner::find($id);
+
+            if($request->has('photo')) {
+                // delete old partner logo from filesystem
+                \File::delete(basename($partner->photo));
+
+                // store new partner logo image to public/images folder
+                $new_logo_path = $request->photo->store('public/images');
+
+                // get partner logo image from public/images folder
+                $new_partner_logo = Storage::get($new_logo_path);
+
+                // crop image and save it again with partners name
+                $file= Image::make(
+                    $new_partner_logo)
+                    ->crop('100','100')
+                    ->save($request->name.'.png'); 
+
+                Storage::delete($new_logo_path);
+            }
+
+            $partner->name = $request->name;
+            $partner->photo = 'public/'.$request->name.'.png';
+            $partner->save();
+        }
     }
 
     /**
@@ -94,6 +149,7 @@ class PartnerController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $partner = Partner::find($id);
+        $partner->delete();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -30,35 +30,29 @@ use App\Http\Controllers\PartnerController;
 
 
 // 1. GET /api/partners -- return all partners sorted by id field and paginate the results. The results should contain also the full URL to partner logo.
+Route::get('partners', [PartnerController::class, 'index']);
 // 2. GET /api/partners?name=ab&per_page=3&page=2 -- return partners where name starts with prefix "ab".
-Route::get('/partners/{name?}/{per_page?}/{page?}', function() {
-});
+/*Route::get('/partners/{name?}/{per_page?}/{page?}', function($name='John', $per_page=null, $page=null) {
+});*/
 
-/*Route::get('partners/{name?}/{per_page?}/{page?}', [PartnerController::class, 'index']);*/
+Route::get('partners/{name?}/{per_page?}/{page?}', [PartnerController::class, 'index']);
 
 // 3. GET /api/partners/{id} -- return a single partner with the given id.
 /*Route::get('/partners/{id}', function() {
 });*/
 Route::get('partners/{id}', [PartnerController::class, 'show']);
-/*
-// 4. POST /api/partners -- create a new partner, upload a partner logo, crop it to 100x100 pixels and store the filename in the field "photo". Implement a case insensitive rule that the "name" field can't contain the substring "Nexus" or its dashed derivatives (e.g. "N-exus", "nE-x----U-s").
-Route::post('/partners', function() {
 
-    $request->validate([
-        'name' => 'required'
-    ]);
-});
+// 4. POST /api/partners -- a) create a new partner, b) upload a partner logo, c) crop it to 100x100 pixels and store the filename in the field "photo". d) Implement a case insensitive rule that the "name" field can't contain the substring "Nexus" or its dashed derivatives (e.g. "N-exus", "nE-x----U-s").
+Route::post('partners', [PartnerController::class, 'store']);
+
 
 // 5. PATCH /api/partners/{id} -- allow to update the partner info including the logo, the same validation rules apply as before.
-Route::patch('/partners/{id}', function() {
-});
+Route::post('partners/{id}', [PartnerController::class, 'update']);
+
 
 // 6. DELETE /api/partners/{id} -- delete the partner with the given id.
-Route::delete('/partners/{id}', function() {
-});
-*/
+Route::delete('partners/{id}', [PartnerController::class, 'delete']);
 
-//Route::resource('partners', PartnerController::class);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
